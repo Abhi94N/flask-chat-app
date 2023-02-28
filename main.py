5fbfbb56e282289a2686499b5a441760ca5e00cc
Abhilash Nair <abhilashnair1994@gmail.com>
2023-02-28 15:10:55 -0500

    added templates, block content, and basic styling

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
 
-app - Flask(__name__)
+app = Flask(__name__)
 app.config["SECRET_KEY"] = "kasjfdldk"
-socket.io = SocketIO(app)
+socketio = SocketIO(app)
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
